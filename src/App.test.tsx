e2b2bcccb40187df7c5d470d6ae6a7d4e95fa8a4
Montacer Dkhilali <montacer.dkhilali@gmail.com>
2021-04-29 10:13:57 +0100

    Add counter state failed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,26 @@
+import { configure, shallow, ShallowWrapper } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { configure, shallow } from "enzyme";
 
 import App from "./App";
 
 configure({ adapter: new Adapter() });
 
 describe("App Component", () => {
+  let wrapper: ShallowWrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   test("renders counter page title", () => {
-    const wrapper = shallow(<App />);
     // console.log(wrapper.debug());
     expect(wrapper.find("h1").text()).toContain("This is a counter app.");
   });
 
   test("renders a button with increment text", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find("#increment-button").text()).toBe("Increment");
+  });
+
+  test("renders the initial value of a state in a div", () => {
+    expect(wrapper.find("#counter-value").text()).toBe(0);
   });
 });
