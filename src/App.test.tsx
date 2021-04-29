52ada105e8d8b6d3c712053fa7ab67559f364422
Montacer Dkhilali <montacer.dkhilali@gmail.com>
2021-04-29 09:51:52 +0100

    Use Enzyme instead of Reacct Testing Lib

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { configure, shallow } from "enzyme";
 
 import App from "./App";
 
+configure({ adapter: new Adapter() });
+
 describe("App Component", () => {
   test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText("This is a counter app.");
-    expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App />);
+    // console.log(wrapper.debug());
+    expect(wrapper.find("h1").text()).toContain("This is a counter app.");
   });
 });
