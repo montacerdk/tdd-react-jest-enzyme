f9633cecb063390d92b7a67472e902b321c20263
Montacer Dkhilali <montacer.dkhilali@gmail.com>
2021-04-29 10:00:13 +0100

    Adding a new failed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,9 +6,14 @@ import App from "./App";
 configure({ adapter: new Adapter() });
 
 describe("App Component", () => {
-  test("renders learn react link", () => {
+  test("renders counter page title", () => {
     const wrapper = shallow(<App />);
     // console.log(wrapper.debug());
     expect(wrapper.find("h1").text()).toContain("This is a counter app.");
+  });
+
+  test("renders a button with increment text", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("#increment-button").text()).toBe("Increment");
   });
 });
